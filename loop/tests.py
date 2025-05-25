44a59247ca76e966246672c2e465b94d14b2ea37
Mikko Kotila <mailme@mikkokotila.com>
2025-05-25 17:05:01 +0300

    chore: Rename lgb sfm

--- loop/tests.py
+++ loop/tests.py
@@ -1,5 +1,5 @@
 import loop
-from loop.models import ligthgbm
+from loop.models import lightgbm
 
 file_url = 'https://data.binance.vision/data/spot/daily/trades/BTCUSDT/BTCUSDT-trades-2025-05-23.zip'
 
@@ -7,5 +7,5 @@ historical = loop.HistoricalData()
 cols = ['trade_id', 'price', 'quantity', 'quote_quantity', 'timestamp', 'is_buyer_maker',  '_null']
 historical.get_binance_file(file_url, False, cols)
 
-uel = loop.UniversalExperimentLoop(historical.data, ligthgbm)
+uel = loop.UniversalExperimentLoop(historical.data, lightgbm)
 uel.run(experiment_name='test_xx', n_permutations=100)
