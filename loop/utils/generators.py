518a146f75d9295b34d19e42a237053f61c0eb23
Mikko Kotila <mailme@mikkokotila.com>
2025-05-31 11:16:44 +0300

    feat: Add paramater range generator

--- loop/utils/generators.py
+++ loop/utils/generators.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 def generate_permutation(params):
     
     out_dict = {}
@@ -8,3 +9,13 @@ def generate_permutation(params):
         out_dict[key] = np.random.choice(params()[key])
 
     return out_dict
+
+
+def generate_parameter_range(start, stop, step, round_by=3):
+
+    '''Generates a list of rounded values'''
+
+    values = np.arange(start, stop, step).tolist()
+    values = [round(value, round_by) for value in values]
+
+    return values
