adc8d756466b8dad5cd0fdff52d7928ca21d59ba
Mikko Kotila <mailme@mikkokotila.com>
2025-08-13 20:27:48 +0300

    fix: Set backtest results correctly

--- loop/log/__init__.py
+++ loop/log/__init__.py
@@ -1,13 +1,10 @@
 from loop.log.log import Log
+from loop.log._experiment_backtest_results import _experiment_backtest_results
 from loop.log._experiment_confusion_metrics import _experiment_confusion_metrics
 from loop.log._experiment_feature_correlation import _experiment_feature_correlation
 from loop.log._permutation_confusion_metrics import _permutation_confusion_metrics
 from loop.log._permutation_prediction_performance import _permutation_prediction_performance
 
 __all__ = [
-    'Log',
-    'experiment_confusion_metrics',
-    'experiment_feature_correlation',
-    'permutation_confusion_metrics',
-    'permutation_prediction_performance'
+    'Log'
 ]
