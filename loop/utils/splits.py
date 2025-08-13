243dfc93de20a8cc746ab253f915fe6890484819
Mikko Kotila <mailme@mikkokotila.com>
2025-08-13 20:36:45 +0300

    feat: Update split_data_to_prep_output

--- loop/utils/splits.py
+++ loop/utils/splits.py
@@ -67,7 +67,9 @@ def split_random(data: pl.DataFrame, ratios: Sequence[int], seed: int = None) ->
     return [data.sample(fraction=1.0, seed=seed, shuffle=True).slice(start, end - start) for start, end in zip(starts, bounds)]
 
 
-def split_data_to_prep_output(split_data: list, cols: list) -> dict:
+def split_data_to_prep_output(split_data: list,
+                              cols: list,
+                              all_datetimes: list) -> dict:
     
     '''
     Compute data preparation output dictionary from split data and column names.
@@ -80,11 +82,31 @@ def split_data_to_prep_output(split_data: list, cols: list) -> dict:
         dict: Dictionary with train, validation, and test features and targets
     '''
 
-    return {
-        'x_train': split_data[0][cols[:-1]],
-        'y_train': split_data[0][cols[-1]],
-        'x_val': split_data[1][cols[:-1]],
-        'y_val': split_data[1][cols[-1]],
-        'x_test': split_data[2][cols[:-1]],
-        'y_test': split_data[2][cols[-1]],
-    }
+    remaining_datetimes = split_data[0]['datetime'].to_list()
+    remaining_datetimes += split_data[1]['datetime'].to_list()
+    remaining_datetimes += split_data[2]['datetime'].to_list()
+
+    first_test_datetime = split_data[2]["datetime"].min()
+    last_test_datetime = split_data[2]["datetime"].max()
+
+    split_data[0] = split_data[0].drop("datetime")
+    split_data[1] = split_data[1].drop("datetime")
+    split_data[2] = split_data[2].drop("datetime")
+
+    if "datetime" in cols:
+        cols.remove("datetime")
+            
+    data_dict = {'x_train': split_data[0][cols[:-1]],
+                 'y_train': split_data[0][cols[-1]],
+                 'x_val': split_data[1][cols[:-1]],
+                 'y_val': split_data[1][cols[-1]],
+                 'x_test': split_data[2][cols[:-1]],
+                 'y_test': split_data[2][cols[-1]]}
+
+    data_dict['_alignement'] = {}
+    
+    data_dict['_alignement']['missing_datetimes'] = sorted(set(all_datetimes) - set(remaining_datetimes))
+    data_dict['_alignement']['first_test_datetime'] = first_test_datetime
+    data_dict['_alignement']['last_test_datetime'] = last_test_datetime
+    
+    return data_dict
