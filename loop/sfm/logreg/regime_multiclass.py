4641e3ba631796009257b97e85aab32290c2e22b
Arun R <arun@ftltech.io>
2025-08-14 12:48:56 +0530

    fix: Sfm > logreg > add utils helper funcs

--- loop/sfm/logreg/regime_multiclass.py
+++ loop/sfm/logreg/regime_multiclass.py
@@ -17,8 +17,8 @@ from datetime import timedelta
 
 # Import parts of our own libraries (leave empty line above)
 from loop.utils.splits import split_sequential, split_data_to_prep_output
-from loop.sfm.lightgbm.utils.regime_multiclass import build_sample_dataset_for_regime_multiclass
-from loop.sfm.lightgbm.utils.regime_multiclass import add_features_to_regime_multiclass_dataset
+from loop.sfm.logreg.utils.regime_multiclass import build_sample_dataset_for_regime_multiclass
+from loop.sfm.logreg.utils.regime_multiclass import add_features_to_regime_multiclass_dataset
 from loop.transforms.logreg_transform import LogRegTransform
 from loop.metrics.multiclass_metrics import multiclass_metrics
 
