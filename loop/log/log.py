dcc6b057379f6426c0e1fdae51781ed0e8621a68
Soultan <soultntoure@gmail.com>
2025-09-24 09:27:00 +0800

    trying to fix debug

--- loop/log/log.py
+++ loop/log/log.py
@@ -67,35 +67,38 @@ class Log:
             self.inverse_scaler = None
 
     def _get_test_data_with_all_cols(self, round_id: int) -> pl.DataFrame:
-        print(f"Log._get_test_data_with_all_cols called for round_id={round_id}")
+        
+        '''
+        Compute test-period rows with all columns.
+        
+        Args:
+            round_id (int): Round ID
+        
+        Returns:
+            pl.DataFrame: Klines dataset filtered down to the permutation test window
+        '''
+
         missing_datetimes = self._alignment[round_id]['missing_datetimes']
         first_test_datetime = self._alignment[round_id]['first_test_datetime']
         last_test_datetime = self._alignment[round_id]['last_test_datetime']
 
-        # If missing_datetimes is empty, skip join entirely to avoid Cast error
+        data_df = self.data.with_columns(pl.col('datetime').dt.cast_time_unit('ms'))
+
+        # If missing_datetimes is empty, skip join and just filter by test window
         if not missing_datetimes:
-            return (
-                self.data
-                .with_columns(pl.col('datetime').dt.cast_time_unit('ms'))
-                .filter(pl.col('datetime').is_between(first_test_datetime, last_test_datetime, closed='both'))
+            return data_df.filter(
+                pl.col('datetime').is_between(first_test_datetime, last_test_datetime, closed='both')
             )
 
-        # Now print and continue if join will occur
-        print("DEBUG: sample self.data['datetime']: ", self.data['datetime'].head(10))
-        print("DEBUG: null count in self.data['datetime']:", self.data['datetime'].null_count())
-        print("DEBUG: sample missing_datetimes:", missing_datetimes[:10] if isinstance(missing_datetimes, list) else missing_datetimes)
-        print("DEBUG: is missing_datetimes empty?", not bool(missing_datetimes))
-        if isinstance(missing_datetimes, list) and missing_datetimes:
-            print("DEBUG: missing_datetimes null count:", sum(x is None for x in missing_datetimes))
+        # If not empty, do the join
+        missing_df = pl.DataFrame({'datetime': missing_datetimes})
+        # Only .with_columns if missing_df is not empty!
+        # (This block will only execute if len(missing_datetimes) > 0)
+        if missing_df.shape[0] > 0:
+            missing_df = missing_df.with_columns(pl.col('datetime').dt.cast_time_unit('ms'))
 
         return (
-            self.data
-            .with_columns(pl.col('datetime').dt.cast_time_unit('ms'))
-            .join(
-                pl.DataFrame({'datetime': missing_datetimes})
-                .with_columns(pl.col('datetime').dt.cast_time_unit('ms')),
-                on='datetime',
-                how='anti',
-            )
+            data_df
+            .join(missing_df, on='datetime', how='anti')
             .filter(pl.col('datetime').is_between(first_test_datetime, last_test_datetime, closed='both'))
         )
