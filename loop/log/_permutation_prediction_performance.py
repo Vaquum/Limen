a1694504b9cf622c0c69384d3c3b3c1b3842438c
Soultan <soultntoure@gmail.com>
2025-09-24 14:18:28 +0800

    fixing permutaton pred

--- loop/log/_permutation_prediction_performance.py
+++ loop/log/_permutation_prediction_performance.py
@@ -21,21 +21,27 @@ def _permutation_prediction_performance(self,
             round_data = self.prep(self.data, self.round_params[round_id])
     except TypeError:
         round_data = self.prep(self.data)
-    
+
+    # Optionally produce perf_df from inverse scaler
     if self.inverse_scaler is not None:
         perf_df = self.inverse_scaler(round_data['x_test'], self.scalers[round_id]).to_pandas()
-    
     else:
         perf_df = pd.DataFrame()
-    
-    perf_df['predictions'] = self.preds[round_id]
-    perf_df['actuals'] = round_data['y_test']
-    perf_df['hit'] = perf_df['predictions'] == perf_df['actuals']
-    perf_df['miss'] = perf_df['predictions'] != perf_df['actuals']
 
+    # Scores (windowed length)
+    perf_df['predictions'] = self.preds[round_id]
+    perf_df['actuals']     = round_data['y_test']
+    perf_df['hit']         = perf_df['predictions'] == perf_df['actuals']
+    perf_df['miss']        = perf_df['predictions'] != perf_df['actuals']
+
+    # Grab price columns and align to windowed length
     price_df = self._get_test_data_with_all_cols(round_id)
-    perf_df['open'] = price_df['open']
-    perf_df['close'] = price_df['close']
-    perf_df['price_change'] = price_df['close'] - price_df['open']
+    # Always slice from the end to match windowed outputs
+    N = len(perf_df)
+    perf_df['open']        = price_df['open'][-N:].reset_index(drop=True)
+    perf_df['close']       = price_df['close'][-N:].reset_index(drop=True)
+    perf_df['price_change'] = perf_df['close'] - perf_df['open']
+
+    # If you want other columns, do the same: perf_df['col'] = price_df['col'][-N:].reset_index(drop=True)
 
     return perf_df
