faa6233ab32d38d7035557dc8a34d41333bc3b7f
weida0715 <ng62312@gmail.com>
2025-10-06 14:42:03 +0800

    update changelog and setup file versioning

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='loop',
-    version='1.20.1',
+    version='1.19.4',
     packages=find_packages(),
     install_requires=[],
     python_requires='>=3.10',
