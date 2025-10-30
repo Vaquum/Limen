4d3ee3a165f0dfc058bc55c8c59665ddec812ca3
Arun R <arun@ftltech.io>
2025-10-30 18:20:09 +0530

    chore: bump version to 1.20.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='loop',
-    version='1.20.2',
+    version='1.20.3',
     packages=find_packages(),
     install_requires=[],
     python_requires='>=3.10',
