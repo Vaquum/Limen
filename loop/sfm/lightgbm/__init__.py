b0d025535ca9079be1d78fdd1cdab504619bba20
Arun R <arun@ftltech.io>
2025-11-18 23:51:42 +0530

    feat: add tradeline directional conditional SFM

--- loop/sfm/lightgbm/__init__.py
+++ loop/sfm/lightgbm/__init__.py
@@ -3,6 +3,7 @@
 import loop.sfm.lightgbm.tradeable_regressor as tradeable_regressor
 import loop.sfm.lightgbm.tradeline_multiclass as tradeline_multiclass
 import loop.sfm.lightgbm.tradeline_long_binary as tradeline_long_binary
+import loop.sfm.lightgbm.tradeline_directional_conditional as tradeline_directional_conditional
 import loop.sfm.lightgbm.utils as utils
 
 
@@ -10,6 +11,7 @@ __all__ = [
     'tradeable_regressor',
     'tradeline_multiclass',
     'tradeline_long_binary',
+    'tradeline_directional_conditional',
     'utils'
 ]
 
--- loop/tests/test_sfm.py
+++ loop/tests/test_sfm.py
@@ -70,6 +70,7 @@ def test_sfm():
         (sfm.lightgbm.tradeable_regressor, get_klines_data_large, False, False),
         (sfm.lightgbm.tradeline_long_binary, get_klines_data_fast, True, False),
         (sfm.lightgbm.tradeline_multiclass, get_klines_data_fast, True, False),
+        (sfm.lightgbm.tradeline_directional_conditional, get_klines_data_fast, True, False),
         (sfm.rules_based.momentum_volatility_longonly,
          get_klines_data_small_fast, True, False),
         (sfm.rules_based.momentum_volatility,
