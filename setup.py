cda674b4aeb5d64c7dc87e4860e288aac0805917
Jatin Thakur <jtkalga@gmail.com>
2025-12-12 12:05:34 +0530

    misc: rebased with main

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='loop',
+    version='1.25.0',
+    packages=find_packages(),
+    install_requires=[],
+    python_requires='>=3.10',
+    author='Mikko Kotila',
+    author_email='mailme@mikkokotila.com',
+    description='Bitcoin-first research and trading platform.',
+)
