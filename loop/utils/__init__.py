126da9fa5080cd70738edf12f21469562f82f96c
pdey <prasenjit.dey@gmail.com>
2025-12-24 17:11:10 +0530

    chore: remove unnecessary utils

--- loop/utils/__init__.py
+++ loop/utils/__init__.py
@@ -1,4 +1,3 @@
-from loop.utils.generators import generate_permutation, generate_parameter_range
 from loop.utils.generic_endpoint_for_tdw import generic_endpoint_for_tdw
 from loop.utils.get_klines_data import get_klines_data
 from loop.utils.get_trades_data import get_trades_data
@@ -9,26 +8,20 @@ from loop.utils.param_space import ParamSpace
 from loop.utils.random_slice import random_slice
 from loop.utils.reporting import format_report_header, format_report_section, format_report_footer
 from loop.metrics.safe_ovr_auc import safe_ovr_auc
-from loop.utils.scale_data_dict import scale_data_dict
 from loop.utils.splits import split_data_to_prep_output, split_sequential, split_random
-from loop.utils.breakout_labeling import to_average_price_klines, compute_htf_features, build_breakout_flags
 from loop.utils.confidence_filtering_system import confidence_filtering_system
 from loop.utils.add_breakout_ema import add_breakout_ema
 from loop.utils.binance_file_to_polars import binance_file_to_polars
-from loop.utils.slice_time_series import slice_time_series
 from loop.utils.data_sampling.full_dataset_sampling import full_dataset_sampling
 from loop.utils.data_sampling.random_subsets_sampling import random_subsets_sampling
 from loop.utils.data_sampling.bootstrap_sampling import bootstrap_sampling
 from loop.utils.data_sampling.temporal_windows_sampling import temporal_windows_sampling
-from loop.utils.uel_split_megamodel import uel_split_megamodel
 from loop.utils.shift_column import shift_column
 
 __all__ = [
     'add_breakout_ema',
     'binance_file_to_polars',
     'bootstrap_sampling',
-    'build_breakout_flags',
-    'compute_htf_features',
     'confidence_filtering_system',
     'continuous_metrics',
     'binary_metrics',
@@ -36,8 +29,6 @@ __all__ = [
     'format_report_header',
     'format_report_section',
     'full_dataset_sampling',
-    'generate_parameter_range',
-    'generate_permutation',
     'generic_endpoint_for_tdw',
     'get_klines_data',
     'get_trades_data',
@@ -46,13 +37,9 @@ __all__ = [
     'random_slice',
     'random_subsets_sampling',
     'safe_ovr_auc',
-    'scale_data_dict',
-    'slice_time_series',
     'split_data_to_prep_output',
     'split_random',
     'split_sequential',
     'temporal_windows_sampling',
-    'to_average_price_klines',
-    'uel_split_megamodel',
     'shift_column'
 ] 
