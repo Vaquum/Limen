28306a2533004ebf3690cfcaf5a8c6dd0a43989c
Arun R <arun@ftltech.io>
2025-12-25 21:29:24 +0530

    style: group reference imports together in sfm/__init__.py

--- loop/sfm/__init__.py
+++ loop/sfm/__init__.py
@@ -1,15 +1,16 @@
 import loop.sfm.reference.decomposition as decomposition
 import loop.sfm.reference.empty as empty
 import loop.sfm.reference.lightgbm as lightgbm_reference
-import loop.sfm.lightgbm as lightgbm
-import loop.sfm.reference.random as random
-import loop.sfm.reference.xgboost as xgboost
 import loop.sfm.reference.logreg as logreg_reference
-import loop.sfm.logreg as logreg
-import loop.sfm.reference as reference
-import loop.sfm.rules_based as rules_based
-import loop.sfm.ridge as ridge
+import loop.sfm.reference.random as random
 import loop.sfm.reference.tabpfn as tabpfn
+import loop.sfm.reference.xgboost as xgboost
+import loop.sfm.reference as reference
+
+import loop.sfm.lightgbm as lightgbm
+import loop.sfm.logreg as logreg
+import loop.sfm.ridge as ridge
+import loop.sfm.rules_based as rules_based
 
 __all__ = [
     'decomposition',
