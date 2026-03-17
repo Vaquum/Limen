cbaea9e10796bae024fb170169858ca43d0ec0ee
Pro <prasenjit.dey@gmail.com>
2026-03-17 17:04:50 +0530

    feat: export Trainer and Sensor from package

--- limen/__init__.py
+++ limen/__init__.py
@@ -2,7 +2,10 @@ from limen.data import HistoricalData
 from limen.log.log import Log
 from limen.trading import Account
 from limen.backtest.backtest_sequential import BacktestSequential
-from limen.experiment import UniversalExperimentLoop, Manifest
+from limen.experiment import Manifest
+from limen.experiment import Sensor
+from limen.experiment import Trainer
+from limen.experiment import UniversalExperimentLoop
 from limen.cohort import RegimeDiversifiedOpinionPools
 
 from limen import features
@@ -21,6 +24,8 @@ __all__ = [
     'Log',
     'Manifest',
     'RegimeDiversifiedOpinionPools',
+    'Sensor',
+    'Trainer',
     'UniversalExperimentLoop',
     'features',
     'indicators',
--- limen/experiment/__init__.py
+++ limen/experiment/__init__.py
@@ -1,7 +1,11 @@
 from limen.experiment.experiment_core import UniversalExperimentLoop
 from limen.experiment.manifest_core import Manifest
+from limen.experiment.sensor import Sensor
+from limen.experiment.trainer import Trainer
 
 __all__ = [
     'Manifest',
+    'Sensor',
+    'Trainer',
     'UniversalExperimentLoop',
 ]
