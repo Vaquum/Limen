170239753ea0645a1841b05f97a1207059848c44
Pro <prasenjit.dey@gmail.com>
2026-03-20 12:43:08 +0530

    feat: add ReconstructionError exception

--- limen/experiment/__init__.py
+++ limen/experiment/__init__.py
@@ -1,10 +1,12 @@
 from limen.experiment.experiment_core import UniversalExperimentLoop
 from limen.experiment.manifest_core import Manifest
+from limen.experiment.trainer import ReconstructionError
 from limen.experiment.trainer import Sensor
 from limen.experiment.trainer import Trainer
 
 __all__ = [
     'Manifest',
+    'ReconstructionError',
     'Sensor',
     'Trainer',
     'UniversalExperimentLoop',
