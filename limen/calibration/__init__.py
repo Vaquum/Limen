ab7d9059ae3415e4b35f9df7a3e962160a2fa0de
Pro <prasenjit.dey@gmail.com>
2026-06-02 00:17:22 +0530

    feat: add fit_calibrator to calibration pipeline and export from package

--- limen/calibration/__init__.py
+++ limen/calibration/__init__.py
@@ -1,6 +1,7 @@
 from limen.calibration.pipeline import CalibratorProtocol
 from limen.calibration.pipeline import ThresholdOptimizerProtocol
 from limen.calibration.pipeline import apply_calibrated_predict
+from limen.calibration.pipeline import fit_calibrator
 from limen.calibration.probability import sklearn_probability_calibrator
 from limen.calibration.threshold import grid_threshold_optimizer
 
@@ -8,6 +9,7 @@ __all__ = [
     'CalibratorProtocol',
     'ThresholdOptimizerProtocol',
     'apply_calibrated_predict',
+    'fit_calibrator',
     'grid_threshold_optimizer',
     'sklearn_probability_calibrator',
 ]
